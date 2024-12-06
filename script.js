8b6898606d758eb158e97e1aa6277562ce6c951b
Deanna Mandarino <mandarinodeanna@gmail.com>
2024-12-05 19:24:37 -0500

    Added removeTransition and added click

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-window.addEventListener('keydown', function(event) {
-  const audio = document.querySelector(`audio[id="sound-${event.key}"]`);
-  const key = document.querySelector(`.key[id="key-${event.key}"]`);
+function playSound(event) {
+  const keyElement = event.target.closest('.key'); // Get the closest parent key element
+  const key = keyElement ? keyElement.getAttribute('id') : null;
+  const audio = document.querySelector(`#sound-${key}`);
 
   if (!audio) {
     return; //stops function from running
@@ -8,4 +9,22 @@ window.addEventListener('keydown', function(event) {
 
   audio.currentTime = 0; //rewinds to the start
   audio.play();
+
+  keyElement.classList.add('playing');
+}
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') {
+    return; //skips if it is not a transform
+  }
+
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+  key.addEventListener('click', playSound);
+  key.addEventListener('transitionend', removeTransition);
 });
+
+window.addEventListener('keydown', playSound);
